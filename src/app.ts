dc0dfce428d8b67c4c3a6b38b2de85a2504b9ff4
earthpywt <piyawat.kus@gmail.com>
2024-11-08 14:45:25 +0700

    add dockerfile and docker compose

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // For parsing JSON bodies
 app.use('/api', reviewRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
